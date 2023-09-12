1980d8a714f3dbe11ddb3f104f511ba7abfc0fcd
Toluwani Okunola <112892425+toluwanimi02@users.noreply.github.com>
2023-09-12 22:26:59 +0000

    removed line 12 - 15

--- src/app.js
+++ src/app.js
@@ -9,7 +9,3 @@ if (sumOfNumbersTo && typeof sumOfNumbersTo === 'function'){
 if (countEvenNumbers && typeof countEvenNumbers === 'function'){
     console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 }
-
-if (celsiusToFahrenheit && typeof celsiusToFahrenheit === 'function'){
-    console.log(celsiusToFahrenheit([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-}
